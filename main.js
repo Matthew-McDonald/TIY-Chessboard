3e166382424337ecfdf4f42d7e2c30cf9c620eb7
Matt McDonald <mattmcd1221@gmail.com>
2017-06-13 15:50:44 -0400

    add id to html and create array in js

---
+++ main.js
@@ -0,0 +1,14 @@
+//set id's for images and link to javascript
+//create array of the board using image variables
+// Link step forward button to first movement
+
+
+var board = [
+  ['BC','BKN','BB','BQ','BK','BB','BKN','BC'],
+  ['BP','BP','BP','BP','BP','BP','BP','BP'],
+  [' ',' ',' ',' ',' ',' ',' ',' '],
+  [' ',' ',' ',' ',' ',' ',' ',' '],
+  [' ',' ',' ',' ',' ',' ',' ',' '],
+  [' ',' ',' ',' ',' ',' ',' ',' '],
+  ['WP','WP','WP','WP','WP','WP','WP','WP'],
+  ['WC','WKN','WB','WQ','WK','WB','WKN','WC'] ]
